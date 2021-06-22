12af4d4bb4d8b1be4876b825105c4e8910e9e23a
minah0102 <minah0102@gmail.com>
2021-06-22 13:17:14 -0400

    init_db, created users table

--- db/init_db.js
+++ db/init_db.js
@@ -29,9 +29,12 @@ async function createTables() {
     await client.query(/*sql*/`
           CREATE TABLE users(
             id SERIAL PRIMARY KEY,
-            
+            username VARCHAR(255) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            isAdmin BOOLEAN DEFAULT false
           );
-      `);
+    `);
 
     await client.query(/*sql*/`
         CREATE TABLE products(
