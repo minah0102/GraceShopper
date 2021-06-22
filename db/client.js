293980aadcbc32ff61e8e54d2d37ba0b8d1a0ce6
iparkgood <zuzu8723@gmail.com>
2021-06-22 14:52:12 -0400

    create the rest of the tables, create client.js

---
+++ db/client.js
@@ -0,0 +1,8 @@
+// Connect to DB
+const { Client } = require("pg");
+const DB_NAME = "catnip-dev";
+const DB_URL =
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
+const client = new Client(DB_URL);
+
+module.exports = client;
