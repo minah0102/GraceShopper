1a3dd28487e2858c72b3b56a1f9679a71b9bf782
austin1052 <77468412+austin1052@users.noreply.github.com>
2021-06-24 01:07:57 -0400

    Delete client.js

--- db/client.js
+++
@@ -1,8 +0,0 @@
-// Connect to DB
-const { Client } = require("pg");
-const DB_NAME = "catnip-dev";
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-const client = new Client(DB_URL);
-
-module.exports = client;
