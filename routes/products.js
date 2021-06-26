228aea6747f3963c2d7cf940d17ecc3e9ccb98f3
austin1052 <austin1052@gmail.com>
2021-06-26 01:19:56 -0400

    Finished product routes

--- routes/products.js
+++ routes/products.js
@@ -6,19 +6,68 @@ const {
   getProductById,
   createProduct,
   deleteProduct,
-  getProductReviews,
 } = require("../db/products");
 
-productsRouter.get('/', async (req, res, next) => {
+productsRouter.get("/", async (req, res, next) => {
   try {
     const products = await getAllProducts();
-    console.log("PRODUCTS", products);
     res.send(products);
   } catch (error) {
-    console.error("GET /products error")
+    console.error("GET /products error");
     next(error);
   }
-})
+});
 
+// will need to add requireUser
+productsRouter.get("/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+    const product = await getProductById(productId);
+    if (!product)
+      res.send({
+        error: "Missing Product Error",
+        message: "No product found with that ID",
+      });
+    res.send(product);
+  } catch (error) {
+    console.error("GET /products/:productId error");
+    next(error);
+  }
+});
+
+// will need to add requireAdmin
+productsRouter.post("/", async (req, res, next) => {
+  try {
+    const { name, description, price, quantity } = req.body;
+    let imageName = req.body.imageName ? imageName : "dog.jpeg";
+    const product = await createProduct({
+      name,
+      description,
+      price,
+      quantity,
+      imageName,
+    });
+    res.send(product);
+  } catch (error) {
+    console.error("GET /products/:productId error");
+    next(error);
+  }
+});
+
+productsRouter.delete("/:productId", async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+    const product = await deleteProduct(productId);
+    if (!product)
+      res.send({
+        error: "Missing Product Error",
+        message: "No product found with that ID",
+      });
+    res.send(product);
+  } catch (error) {
+    console.error("DELETE /products/:productId error");
+    next(error);
+  }
+});
 
 module.exports = productsRouter;
