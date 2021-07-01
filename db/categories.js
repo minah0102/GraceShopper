07ac3f235c9a4836f279c542487a647f9848539e
austin1052 <77468412+austin1052@users.noreply.github.com>
2021-06-30 20:41:34 -0400

    Update categories.js

--- db/categories.js
+++ db/categories.js
@@ -35,7 +35,7 @@ const deleteCategory = async (categoryId) => {
     const {
       rows: category
     } = await client.query(/*sql*/`
-      DELETE FROM categories WHERE id=$1;
+      DELETE FROM categories WHERE id=$1 RETURNING *;
     `, [categoryId])
     return category;
   } catch (error) {
