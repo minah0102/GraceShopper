4c8220e7182c90ccee91dba731de12f8c4c546b0
iparkgood <zuzu8723@gmail.com>
2021-07-03 11:15:55 -0400

    add router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Header } from "./components";
+import { Header, Checkout } from "./components";
 
 const App = () => {
   return (
     <div id="app">
-      <Header/>
-      <main>
-      </main>
+      <Router>
+        <Header />
+        <main>
+          <Switch>
+            <Route path="/cart">
+              <Checkout />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   );
 };
