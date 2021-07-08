3b2d0333f76e3a40f8296cab7d50f9d21af9ae78
minah0102 <minah0102@gmail.com>
2021-07-08 13:47:57 -0400

    working login

--- src/components/LoggedInPage.js
+++ src/components/LoggedInPage.js
@@ -1,21 +1,15 @@
 import React, { useContext } from "react";
-import { UserContext } from "./MainAuth";
+import { UserContext } from "..";
 import RecentOrders from "./RecentOrders";
 import Profile from "./Profile";
 import { Button } from "react-bootstrap";
 
 const LoggedInPage = () => {
-  const { user, setUser } = useContext(UserContext);
-
-  const handleLogout = () => {
-    localStorage.clear();
-    setUser(null);
-  };
+  const { user } = useContext(UserContext);
 
   return (
     <div>
       <h1>Welcome {user.username}</h1>
-      <Button onClick={handleLogout}>Logout</Button>
       <RecentOrders />
       <Profile />
     </div>
