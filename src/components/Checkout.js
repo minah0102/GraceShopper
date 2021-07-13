f7c55579f9fe98d6553467fa4785d1a41816eda8
iparkgood <zuzu8723@gmail.com>
2021-07-13 17:41:48 -0400

    modified for both authenticated and unauthenticated

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,61 +3,54 @@ import { ListGroup, Row, Col } from "react-bootstrap";
 import { UserContext } from "..";
 
 const Checkout = () => {
-  const { orderHistory } = useContext(UserContext);
-  const inactiveOrder = orderHistory[orderHistory.length - 1];
+  const { orderHistory, currentUsername, localCart } = useContext(UserContext);
 
-  const total = inactiveOrder.products.reduce(
-    (acc, p) => {
-      return acc + p.quantity * p.price;
-    },
-    0
-  );
+  const renderProducts = currentUsername
+    ? orderHistory[orderHistory.length - 1].products
+    : localCart;
+
+  const total = renderProducts.reduce((acc, p) => {
+    return acc + p.quantity * p.price;
+  }, 0);
+
+  const orderHeader = renderProducts[0].orderId
+    ? `Order #${renderProducts[0].orderId}`
+    : ``;
 
   return (
     <>
       <h2>Thank you for shopping!</h2>
       <ListGroup>
-        <h3>Order #{inactiveOrder.id}</h3>
-        {inactiveOrder.products.map(
-          ({
-            productId,
-            price,
-            quantity,
-            name,
-            description,
-            imageName,
-            orderId,
-            lineItemId,
-          }) => {
-            return (
-              <ListGroup.Item>
-                <Row
-                  style={{
-                    alignItems: "center",
-                  }}
+        <h3>{orderHeader}</h3>
+        {renderProducts.map(({ price, quantity, name, imageName }) => {
+          return (
+            <ListGroup.Item>
+              <Row
+                style={{
+                  alignItems: "center",
+                }}
+              >
+                <Col xs={3}>
+                  <img src={`/images/${imageName}`} width="120px" />
+                </Col>
+                <Col
+                  style={{ display: "flex", justifyContent: "space-between" }}
                 >
-                  <Col xs={3}>
-                    <img src={`/images/${imageName}`} width="120px" />
-                  </Col>
-                  <Col
-                    style={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    {name}
-                    <span style={{ color: "red" }}>
-                      ${Number.parseInt(price).toFixed(2)}
-                    </span>
-                  </Col>
-                  <Col
-                    xs={3}
-                    style={{ display: "flex", justifyContent: "center" }}
-                  >
-                    {quantity}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            );
-          }
-        )}
+                  {name}
+                  <span style={{ color: "red" }}>
+                    ${Number.parseInt(price).toFixed(2)}
+                  </span>
+                </Col>
+                <Col
+                  xs={3}
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  {quantity}
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          );
+        })}
         <h3 style={{ display: "flex", justifyContent: "flex-end" }}>
           Total:{" "}
           <span style={{ color: "red", marginLeft: "20px" }}>
