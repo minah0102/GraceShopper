7b71645f5c2ecca65c35c7543604b4b1eb011964
iparkgood <zuzu8723@gmail.com>
2021-07-14 15:48:31 -0400

    removed Containter because there is one in index

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { UserContext } from "..";
 
@@ -32,35 +32,31 @@ const Header = () => {
   return (
     <>
       <Navbar expand="md">
-        <Container>
-          <Nav.Link onClick={() => history.push("/")}>
-            <h2 style={{ color: "black" }}>Catnip Corp.</h2>
-          </Nav.Link>
-          <Navbar.Toggle aria-controls="main-nav" />
-          <Navbar.Collapse id="main-nav" className="justify-content-end">
-            <Nav>
-              {currentUsername ? (
-                <Nav.Link onClick={() => history.push("/authenticated")}>
-                  Account
-                </Nav.Link>
-              ) : (
-                <Nav.Link onClick={() => history.push("/register")}>
-                  Sign up
-                </Nav.Link>
-              )}
+        <Nav.Link onClick={() => history.push("/")}>
+          <h2 style={{ color: "black" }}>Catnip Corp.</h2>
+        </Nav.Link>
+        <Navbar.Toggle aria-controls="main-nav" />
+        <Navbar.Collapse id="main-nav" className="justify-content-end">
+          <Nav>
+            {currentUsername ? (
+              <Nav.Link onClick={() => history.push("/authenticated")}>
+                Account
+              </Nav.Link>
+            ) : (
+              <Nav.Link onClick={() => history.push("/register")}>
+                Sign up
+              </Nav.Link>
+            )}
 
-              {currentUsername ? (
-                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-              ) : (
-                <Nav.Link onClick={() => history.push("/login")}>
-                  Log in
-                </Nav.Link>
-              )}
+            {currentUsername ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <Nav.Link onClick={() => history.push("/login")}>Log in</Nav.Link>
+            )}
 
-              <Nav.Link onClick={() => history.push("/cart")}>Cart</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+            <Nav.Link onClick={() => history.push("/cart")}>Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
