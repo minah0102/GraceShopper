bc4f58b5d936a6f4394a4ea75a802968a7325557
minah0102 <minah0102@gmail.com>
2021-07-14 20:16:17 -0400

    fixed homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,39 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-
+import "./Home.css";
+import aver from "../images/aver2.jpg";
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Catnip Corp.</h1>
+      <h1 style={{textAlign: "center"}}>Welcome to Catnip Corp.</h1>
       <Carousel>
         <Carousel.Item interval={2000}>
           <img
             className="d-block w-100"
-            src={`/images/donate.jpeg`}
+            src={aver}
             alt="First Slide"
           />
         </Carousel.Item>
 
         <Carousel.Item interval={2000}>
           <img className="d-block w-100"
-            src={`/images/aver2.jpg`}
+            src={aver}
             alt="Second Slide"
           />
         </Carousel.Item>
 
         <Carousel.Item interval={2000}>
           <img className="d-block w-100"
-            src={`/images/aver2.jpg`}
-            alt="third Slide"
+            src={aver}
+            alt="Third Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item interval={2000}>
+          <img className="d-block w-100"
+            src={aver}
+            alt="Fourth Slide"
           />
         </Carousel.Item>
       </Carousel>
