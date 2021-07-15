c2f39f83722f43bbf394a35c5b55d3f1cd1d1d7a
austin1052 <austin1052@gmail.com>
2021-07-14 20:25:48 -0400

    merge

--- src/components/LoggedInPage.js
+++ src/components/LoggedInPage.js
@@ -2,24 +2,15 @@ import React, { useContext } from "react";
 import { UserContext } from "..";
 import RecentOrders from "./RecentOrders";
 import Profile from "./Profile";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-import '../css/User.css'
-=======
 import { Col, Container, Row } from "react-bootstrap";
->>>>>>> efe639c605a7c15c310389d9588fdd8a29045c25
+import '../css/User.css'
 
 const LoggedInPage = () => {
-  const { user } = useContext(UserContext);
+  const { user, currentUsername } = useContext(UserContext);
 
   return (
-<<<<<<< HEAD
     <div id="welcome-user">
       <h1>Welcome {currentUsername}</h1>
-=======
-    <div>
-      <h1>Welcome {user.username}</h1>
->>>>>>> efe639c605a7c15c310389d9588fdd8a29045c25
       <Container>
         <Row>
           <Col xs={8}><RecentOrders /></Col>
