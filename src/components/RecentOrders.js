e40a706351e9455ed9ea7b5ea98f1f8fb27d2323
iparkgood <zuzu8723@gmail.com>
2021-07-16 11:06:02 -0400

    add some style

--- src/components/RecentOrders.js
+++ src/components/RecentOrders.js
@@ -1,26 +1,19 @@
-import React, { useContext } from "react";
-import {
-  Container,
-  Table,
-  Row,
-  Col,
-  Accordion,
-  Card,
-  ListGroup,
-} from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Row, Col, Accordion, Card, ListGroup } from "react-bootstrap";
 import { UserContext } from "..";
 
 const cardHeaderStyle = {
   backgroundColor: "#a59c83",
-  
-}
+};
 
 const RecentOrders = () => {
   const { orderHistory } = useContext(UserContext);
-  console.log("show me orderHIstory", orderHistory);
+
   return (
     <>
-      <h2 style={{fontSize: "1.5rem", marginBottom: "1rem"}}>Recent Orders</h2>
+      <h2 style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>
+        Recent Orders
+      </h2>
 
       <Accordion defaultActiveKey="0" flush>
         {orderHistory.length > 0 &&
@@ -28,48 +21,64 @@ const RecentOrders = () => {
             return (
               <>
                 <Card.Header style={cardHeaderStyle}>
-                  <Accordion.Toggle 
+                  <Accordion.Toggle
                     as={ListGroup}
                     eventKey={(idx + 1).toString()}
                   >
                     <Row>
-                      <Col md={4}>Order #{o.id}</Col>
-                      <Col>
-                        purchased on {new Date(o.purchasedDate).toLocaleDateString()}
+                      <Col>Order #{o.id}</Col>
+                      <Col style={{ textAlign: "end" }}>
+                        purchased on
+                        {new Date(o.purchasedDate).toLocaleDateString()}
                       </Col>
                     </Row>
                   </Accordion.Toggle>
                 </Card.Header>
-                <Accordion.Collapse style={{backgroundColor: "#fdfbf8"}} eventKey={(idx + 1).toString()}>
+                <Accordion.Collapse
+                  style={{ backgroundColor: "#fdfbf8" }}
+                  eventKey={(idx + 1).toString()}
+                >
                   <>
                     {o.products.length > 0 &&
-                      o.products.map(({ name, price, quantity, imageName }, idx) => {
-                        return (
-                          <Row
-                            style={{
-                              alignItems: "center",
-                            }} key={idx}
-                          >
-                            <Col xs={3}>
-                              <img src={`/images/${imageName}`} width="120px" />
-                            </Col>
-                            <Col
-                              style={{ display: "flex", justifyContent: "space-between" }}
+                      o.products.map(
+                        ({ name, price, quantity, imageName }, idx) => {
+                          return (
+                            <Row
+                              style={{
+                                alignItems: "center",
+                              }}
+                              key={idx}
                             >
-                              {name}
-                              <span style={{ color: "red" }}>
-                                ${Number.parseFloat(price).toFixed(2)}
-                              </span>
-                            </Col>
-                            <Col
-                              xs={3}
-                              style={{ display: "flex", justifyContent: "center" }}
-                            >
-                              {quantity}
-                            </Col>
-                          </Row>
-                        );
-                      })}
+                              <Col xs={3}>
+                                <img
+                                  src={`/images/${imageName}`}
+                                  width="120px"
+                                />
+                              </Col>
+                              <Col
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "space-between",
+                                }}
+                              >
+                                {name}
+                                <span style={{ color: "red" }}>
+                                  ${Number.parseFloat(price).toFixed(2)}
+                                </span>
+                              </Col>
+                              <Col
+                                xs={3}
+                                style={{
+                                  display: "flex",
+                                  justifyContent: "center",
+                                }}
+                              >
+                                {quantity}
+                              </Col>
+                            </Row>
+                          );
+                        }
+                      )}
                   </>
                 </Accordion.Collapse>
               </>
